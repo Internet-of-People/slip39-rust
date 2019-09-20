42f5a35859407b851ca87d45844472eb14a523d9
wigy <wigy_opensource_developer@yahoo.com>
2019-09-20 17:04:59 +0200

    Generating sample splits

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use failure::Fallible;
+use sssmc39::*;
+
+fn main()-> Fallible<()> {
+    let threshold = 2;
+    let groups = generate_mnemonics_random(threshold, &vec![(1, 1), (1, 1), (3, 5), (2, 6)], 256, "morpheus", 4)?;
+    println!("Secret ({}-of-{})", threshold, groups.len());
+    for (g, group) in groups.iter().enumerate() {
+        println!("  Group-{:02} ({}-of-{})", g + 1, group.member_threshold, group.member_shares.len());
+        let mnemonics = group.mnemonic_list()?;
+        for (m, mnemonic) in mnemonics.iter().enumerate() {
+            println!("    Mnemonic-{:02}-{:02}", g + 1, m + 1);
+            for word in mnemonic {
+                let mut left = word.clone();
+                let right = left.split_off(4);
+                println!("      {}·{:·<4}", left, right);
+            }
+        }
+    }
+    Ok(())
+}
