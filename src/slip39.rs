cffb95bdb75cde35e89afd4a4aeb313bfd5df678
wigy <wigy_opensource_developer@yahoo.com>
2019-09-21 17:25:51 +0200

    Split Slip39.display into formatting helpers

--- src/slip39.rs
+++ src/slip39.rs
@@ -14,8 +14,55 @@ impl Slip39 {
 		passphrase: &str,
 		iteration_exponent: u8,
 	) -> Result<Self, Error> {
-		let group_shares = generate_mnemonics(group_threshold, groups, master_secret.as_ref(), passphrase, iteration_exponent)?;
+		let group_shares = generate_mnemonics(
+			group_threshold,
+			groups,
+			master_secret.as_ref(),
+			passphrase,
+			iteration_exponent
+		)?;
 		Ok(Self(group_shares))
+	}
+
+	pub fn iter(&self) -> std::slice::Iter<'_, GroupShare> {
+		self.0.iter()
+	}
+}
+
+struct ShareFormatter<'a>(&'a Share);
+
+impl<'a> fmt::Display for ShareFormatter<'a> {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		let share = self.0;
+		let m = share.member_index + 1;
+		let g = share.group_index + 1;
+		writeln!(f, "Mnemonic-{:02}-{:02}", g, m)?;
+		let mnemonic = share.to_mnemonic().expect("formatting a valid mnemonic should not get an error");
+		let mut i = 0;
+		for row in mnemonic.as_slice().chunks(3) {
+			for word in row {
+				i += 1;
+				let mut left = word.clone();
+				let right = left.split_off(4);
+				write!(f, "  {:02}:{}·{:·<4}", i, left, right)?;
+			}
+			writeln!(f)?;
+		}
+		Ok(())
+	}
+}
+
+struct GroupShareFormatter<'a>(&'a GroupShare);
+
+impl<'a> fmt::Display for GroupShareFormatter<'a> {
+	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+		let group = &self.0;
+		let g = group.group_index + 1;
+		writeln!(f, "Group-{:02} ({}-of-{})", g, group.member_threshold, group.member_shares.len())?;
+		for share in group.member_shares.iter() {
+			writeln!(f, "{}", ShareFormatter(share))?;
+		}
+		Ok(())
 	}
 }
 
@@ -23,23 +70,8 @@ impl fmt::Display for Slip39 {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
 		let share_1_1 = &self.0[0].member_shares[0];
 		writeln!(f, "Secret ({}-of-{})", share_1_1.group_threshold, share_1_1.group_count)?;
-		for (g, group) in self.0.iter().enumerate() {
-			writeln!(f, "  Group-{:02} ({}-of-{})", g + 1, group.member_threshold, group.member_shares.len())?;
-			let mnemonics = group.mnemonic_list().expect("formatting a valid mnemonic should not get an error");
-			for (m, mnemonic) in mnemonics.iter().enumerate() {
-				writeln!(f, "    Mnemonic-{:02}-{:02}", g + 1, m + 1)?;
-				let mut i = 0;
-				for row in mnemonic.as_slice().chunks(3) {
-					write!(f, "    ")?;
-					for word in row {
-						i += 1;
-						let mut left = word.clone();
-						let right = left.split_off(4);
-						write!(f, "  {:02}:{}·{:·<4}", i, left, right)?;
-					}
-					writeln!(f)?;
-				}
-			}
+		for group in self.iter() {
+			writeln!(f, "{}", GroupShareFormatter(group))?;
 		}
 		Ok(())
 	}
