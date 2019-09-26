e147ec941cc957a86202fa48a4e41729189018c4
wigy <wigy_opensource_developer@yahoo.com>
2019-09-26 16:40:35 +0200

    New command combine assembles the master secret from split secrets and outputs it as a BIP39 mnemonic
    For some reason the round-trip for BIP39 -> SLIP39 -> BIP39 does not work well yet.

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,56 @@ mod slip39;
 
 pub use master_secret::MasterSecret;
 pub use slip39::{Slip39, ShareInspector};
-use sssmc39::Share;
+use sssmc39::{Share, combine_mnemonics};
 
 #[derive(Debug, StructOpt)]
 #[structopt(rename_all="kebab")]
-struct Options {
+enum Options {
+	/// Generate master secret and split it to parts
+	///
+	/// SLIP-0039 defines a 2-level split: The master secret is split into group secrets and then
+	/// those are split further into member secrets. You can define the required and total number of
+	/// members in each group, and also define how many groups are required to restore the master secret.
+	Generate {
+		#[structopt(short, long, default_value = "256")]
+		/// Length of the master secret in bits
+		entropy_bits: u16,
+		#[structopt(flatten)]
+		split_options: SplitOptions,
+	},
+	/// Split a master secret encoded as a BIP-0039 mnemonic into parts
+	///
+	/// SLIP-0039 defines a 2-level split: The master secret is split into group secrets and then
+	/// those are split further into member secrets. You can define the required and total number of
+	/// members in each group, and also define how many groups are required to restore the master secret.
+	Split {
+		#[structopt(short, long, env = "SLIP39_BIP39", hide_env_values = true)]
+		/// BIP-0039 mnemonic to split. Use double quotes around it, but preferably provide it
+		/// through an environment variable to avoid leaking it to other processes on this machine
+		mnemonic: String,
+		#[structopt(flatten)]
+		split_options: SplitOptions,
+	},
+	Combine {
+		#[structopt(flatten)]
+		password: Password,
+		#[structopt(short, long("mnemonic"), required=true, number_of_values=1)]
+		mnemonics: Vec<String>,
+	},
+	Inspect {
+		#[structopt(short, long)]
+		/// SLIP-0039 mnemonic to inspect. Use double quotes around it, but preferably provide it
+		/// through an environment variable to avoid leaking it to other processes on this machine
+		mnemonic: String,
+	}
+}
+
+#[derive(Debug, StructOpt)]
+struct Password {
 	#[structopt(short, long, env = "SLIP39_PASSWORD", hide_env_values = true)]
 	/// Password that is required in addition to the mnemonics to restore the master secret. Preferably
 	/// provide it through an environment variable to avoid leaking it to other processes.
 	password: String,
-	#[structopt(subcommand)]
-	sub_command: SubCommand,
 }
 
 fn parse_group_spec(src: &str) -> Fallible<(u8,u8)> {
@@ -30,35 +69,9 @@ fn parse_group_spec(src: &str) -> Fallible<(u8,u8)> {
 }
 
 #[derive(Debug, StructOpt)]
-enum SubCommand {
-	/// Generate master secret and split it to parts
-	/// 
-	/// SLIP-0039 defines a 2-level split: The master secret is split into group secrets and then
-	/// those are split further into member secrets. You can define the required and total number of
-	/// members in each group, and also define how many groups are required to restore the master secret.
-	Generate {
-		#[structopt(short, long, default_value = "256")]
-		/// Length of the master secret in bits
-		entropy_bits: u16,
-		#[structopt(flatten)]
-		split_options: SplitOptions,
-	},
-	Split {
-		#[structopt(short, long, env = "SLIP39_BIP39", hide_env_values = true)]
-		/// BIP-0039 mnemonic to split. Use double quotes around it, but preferably provide it
-		/// through an environment variable to avoid leaking it to other processes on this machine
-		mnemonic: String,
-		#[structopt(flatten)]
-		split_options: SplitOptions,
-	},
-	Combine {},
-	Inspect {
-		mnemonic: String,
-	}
-}
-
-#[derive(Debug, StructOpt)]
 struct SplitOptions {
+	#[structopt(flatten)]
+	password: Password,
 	#[structopt(short, long)]
 	/// Number of groups required for restoring the master secret (by default all groups are required)
 	required_groups: Option<u8>,
@@ -72,38 +85,50 @@ struct SplitOptions {
 }
 
 impl SplitOptions {
-	fn split(&self, master_secret: &MasterSecret, password: &str) -> Fallible<Slip39> {
+	fn split(&self, master_secret: &MasterSecret) -> Fallible<Slip39> {
 		let required_groups = self.required_groups
 			.unwrap_or_else(|| self.groups.len() as u8);
 		let slip39 = Slip39::new(
 			required_groups,
 			&self.groups,
 			&master_secret,
-			&password,
+			&self.password.password,
 			self.iterations
 		)?;
 		Ok(slip39)
 	}
 }
 
+fn print_split(options: &SplitOptions, master_secret: &MasterSecret) -> Fallible<()> {
+	let slip39 = options.split(&master_secret)?;
+	println!("{}", serde_json::to_string_pretty(&slip39)?);
+	Ok(())
+}
+
 fn main() -> Fallible<()> {
+	use Options::*;
 	let options = Options::from_args();
-	match options.sub_command {
-		SubCommand::Generate { entropy_bits, split_options } => {
+	match options {
+		Generate { entropy_bits, split_options } => {
 			let master_secret = MasterSecret::new(entropy_bits)?;
-			let slip39 = split_options.split(&master_secret, &options.password)?;
-			println!("{}", serde_json::to_string_pretty(&slip39)?);
+			print_split(&split_options, &master_secret)?;
 		}
-		SubCommand::Split { mnemonic, split_options } => {
+		Split { mnemonic, split_options } => {
 			let bip39 = Bip39Mnemonic::from_phrase(mnemonic, Language::English)?;
 			let master_secret = MasterSecret::from(&bip39);
-			let slip39 = split_options.split(&master_secret, &options.password)?;
-			println!("{}", serde_json::to_string_pretty(&slip39)?);
+			print_split(&split_options, &master_secret)?;
 		}
-		SubCommand::Combine {} => {
-			println!("Not so fast, young padawan!");
+		Combine { password, mnemonics} => {
+			let mnemonics = mnemonics.iter()
+				.map(|m| {
+					m.split_ascii_whitespace().map(str::to_owned).collect()
+				})
+				.collect();
+			let master_secret = combine_mnemonics(&mnemonics, &password.password)?;
+			let bip39 = bip39::Mnemonic::from_entropy(&master_secret, Language::English)?;
+			println!("{}", bip39.into_phrase());
 		}
-		SubCommand::Inspect { mnemonic } => {
+		Inspect { mnemonic } => {
 			let words = mnemonic.split_ascii_whitespace().map(str::to_owned).collect();
 			let share = Share::from_mnemonic(&words)?;
 			println!("{}", serde_json::to_string_pretty(&ShareInspector::from(&share))?);
